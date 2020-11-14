eaf55e1d82f88a33bcd5344be972cd3ea3d562e1
v.florio <v.florio@reply.it>
2020-11-14 02:33:16 +0100

    Categorie

--- src/convert.js
+++ src/convert.js
@@ -6,7 +6,9 @@ import {
   connect,
   query,
   unixTimestamp,
-  randomColor
+  randomColor,
+  sqlEscape,
+  slugify
 } from "./utils";
 
 // Configurazioni
@@ -33,7 +35,8 @@ const MYSQL_CONFIG = {
 
     // Conversione tabelle
 
-    await migrateUsers(phpbbConnection, flarumConnection);
+    //await migrateUsers(phpbbConnection, flarumConnection);
+    await migrateCategories(phpbbConnection, flarumConnection);
 
     flarumConnection.end();
     phpbbConnection.end();
@@ -49,6 +52,7 @@ const MYSQL_CONFIG = {
   }
 })();
 
+
 export const migrateUsers = (phpbbConnection, flarumConnection) => new Promise(async resolve => {
   console.log(`Migrating users`);
 
@@ -58,21 +62,23 @@ export const migrateUsers = (phpbbConnection, flarumConnection) => new Promise(a
   let migratedUsers = 0;
   let failedUsers = [];
 
-  users.forEach(({ username_clean, user_id, user_regdate, user_email = null }) => {
+  users.forEach((entry) => {
 
-    if (!user_email) {
+    if (!entry.user_email) {
       ignoredUsers++;
       return;
     }
 
+    const { username_clean, user_id, user_regdate, user_email = null } = entry
+    const username = username_clean.replaceAll(" ", "");
     const password = CryptoJS.SHA1(CryptoJS.MD5(unixTimestamp()).toString()).toString();
 
     query(flarumConnection, `
       INSERT INTO ${FLARUM_DB_PREFIX}users (id, username, email, password, joined_at, is_email_confirmed)
-      VALUES ('${user_id}', '${username_clean.replaceAll(" ", "")}', '${user_email}', '${password}', '${user_regdate}', 1)`
-    ).then(result => {
-      if (result) migratedUsers++;
-      else failedUsers.push(entry);
+      VALUES ('${user_id}', '${username}', '${user_email}', '${password}', '${user_regdate}', 1)`
+
+    ).then(() => {
+      migratedUsers++;
 
     }).catch(error => {
       failedUsers.push(entry);
@@ -83,10 +89,61 @@ export const migrateUsers = (phpbbConnection, flarumConnection) => new Promise(a
   let interval = setInterval(() => {
     if (migratedUsers + ignoredUsers + failedUsers.length === users.length) {
       clearInterval(interval);
-      console.log(`Users migration commplete`);
+      console.log(`Users migration completed`);
       console.log({ migratedUsers, ignoredUsers, failedUsers: failedUsers.length });
       resolve();
     }
   }, 500);
 });
 
+export const migrateCategories = (phpbbConnection, flarumConnection) => new Promise(async resolve => {
+
+  console.log(`Converting Categories to tags`);
+
+  const categories = await query(phpbbConnection, `SELECT forum_id, forum_name, forum_desc  FROM ${PHPBB_DB_PREFIX}forums`)
+
+  let migratedCategories = 0;
+  let failedCategories = [];
+
+  categories.forEach((entry, index) => {
+
+    const { forum_id, forum_name, forum_desc } = entry;
+    const position = index;
+    const color = randomColor();
+    const slug = sqlEscape(slugify(forum_name));
+
+    query(flarumConnection, `
+      INSERT INTO ${FLARUM_DB_PREFIX}tags (id, name, description, slug, color, position)
+      VALUES ( '${forum_id}', '${forum_name}', '${forum_desc}', '${slug}', '${color}', '${position}')`
+
+    ).then(() => {
+      migratedCategories++;
+
+    }).catch(() => {
+
+      console.log(`Category '${forum_name}' insert failed, trying update`);
+
+      query(flarumConnection, `
+        UPDATE ${FLARUM_DB_PREFIX}tags SET name = '${forum_name}', description = '${forum_desc}', slug = '${slug}'
+        WHERE id = '${forum_id}'`
+
+      ).then(() => {
+        migratedCategories++;
+
+      }).catch(error => {
+        failedCategories.push(entry);
+        console.log(error?.message);
+      })
+    });
+
+  });
+
+  let interval = setInterval(() => {
+    if (migratedCategories + failedCategories.length === categories.length) {
+      clearInterval(interval);
+      console.log(`Categories migration completed`);
+      console.log({ migratedCategories, failedCategories: failedCategories.length });
+      resolve();
+    }
+  }, 500);
+});
--- src/utils.js
+++ src/utils.js
@@ -31,3 +31,32 @@ export const query = (connection, sql) => new Promise((resolve, reject) => {
 export const unixTimestamp = () => Math.round((new Date()).getTime() / 1000);
 
 export const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+
+export const sqlEscape = (string) =>
+  (typeof string === 'string' || string instanceof String) ?
+    string
+      .replaceAll("\\", "\\\\")
+      .replaceAll("\0", "\\0")
+      .replaceAll("\n", "\\n")
+      .replaceAll("\r", "\\r")
+      .replaceAll("\'", "\\'")
+      .replaceAll('\"', '\\"')
+      .replaceAll("\x1a", "\\Z")
+    : string
+
+// https://gist.github.com/hagemann/382adfc57adbd5af078dc93feef01fe1#file-slugify-js
+export const slugify = (string) => {
+
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
